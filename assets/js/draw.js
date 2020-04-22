a9acfee8c29cb73515be26b12553ed5eba0ce53c
aidant842 <aidant842@gmail.com>
2020-04-22 11:44:12 +0000

    html and css in place, uploaded some assets such as images and sound files, started draw.js

---
+++ assets/js/draw.js
@@ -0,0 +1,8 @@
+//Variables
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+ctx.canvas.width= 300;
+ctx.canvas.height = 300;
+const scale = 20;
+const rows = canvas.height / scale;
+const columns = canvas.width /scale;
