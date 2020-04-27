b35c063fb56f7a0184cad3cdf8becabbdbf60a5f
aidant842 <aidant842@gmail.com>
2020-04-27 12:04:15 +0000

    implemented a fix for the game ending randomly, updated in README

--- assets/js/mouse.js
+++ assets/js/mouse.js
@@ -4,26 +4,17 @@ function Mouse() {
 
     //function for random mouse spawn, get a random integer that isn't outside the canvas and inline with snake
     this.randomMouseSpawn = function () {
-        let count = 0;
-        let valid = true;
-        while (true) {
-            if (count > 100000) {
-                console.log('Game randomly ended');
-                End();
-                break;
-            }
+        let validSpawn = true;
+        while (validSpawn) {
+            validSpawn = false;
             this.x = (Math.floor(Math.random() * rows - 1) + 1) * scale; // get a random integer within the confines of row in increments of 10
             this.y = (Math.floor(Math.random() * columns - 1) + 1) * scale;
             for (let i = 0; i < snake.snakeLength.length; i++) {
                 if (this.x === snake.snakeLength[i].x && this.y === snake.snakeLength[i].y) {
-                    valid = false;
+                    validSpawn = true;
                     break;
                 }
             }
-            if (valid) {
-                break;
-            }
-            count++;
         }
     }
 
